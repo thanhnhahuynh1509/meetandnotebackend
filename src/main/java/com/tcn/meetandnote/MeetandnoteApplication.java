f3ca47ab372cddb36afcc030e7554cc1ae4788ef
Thanh Nha Huynh <thanhnhahuynh1509@gmail.com>
2022-09-27 11:04:48 +0700

    initialize project with basic packages

---
+++ src/main/java/com/tcn/meetandnote/MeetandnoteApplication.java
@@ -0,0 +1,13 @@
+package com.tcn.meetandnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetandnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetandnoteApplication.class, args);
+	}
+
+}
